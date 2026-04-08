07482213e200a2cabfb84aa1d36e732c546d5b71
Vinicios Velasques <viniciosvelasques@gmail.com>
2026-04-08 08:16:12 -0400

    Add files via upload

--- ex1.cpp
+++ ex1.cpp
@@ -1,101 +1,147 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
 #include <map>
-using namespace std;
-using std::map;
+#include <algorithm>
 
-class Eleitor{
+using namespace std;
+
+class Pessoa {
 public:
     int Id, Titulo;
     string Nome;
-    bool Votou;
-
-    Eleitor(int id, int titulo, string nome){
-        Id = id;
-        Titulo = titulo;
-        Nome = nome;
-        Votou = false;
-    }
-
-    void exibir(){
-        cout << "ID: " << Id << " Nome: " << Nome << " com o titulo: " << Titulo << "votou: " << (Votou ? "Sim" : "nao") << endl;
-    }
+    Pessoa(int id, int titulo, string nome) : Id(id), Titulo(titulo), Nome(nome) {}
 };
 
-class Candidato{
-
+class Eleitor : public Pessoa {
 public:
-    int Id, Numero, VotosRecebidos, Titulo;
-    string Nome, Partido;
     bool Votou;
-
-    Candidato(int id, int numero, int titulo, string nome, string partido, bool votou){
-        Id = id;
-        Titulo = titulo;
-        Nome = nome;
-        Votou = false;
-        Partido = partido;
-        VotosRecebidos = 0;
-    }
-    void exibir(){
-        cout << "o candidato " << Nome << " do partido " << Partido << "do numero " << "recebeu " << VotosRecebidos << " votou: " << (Votou ? "Sim" : "Nao") << endl;
+    Eleitor(int id, int titulo, string nome) : Pessoa(id, titulo, nome), Votou(false) {}
+    void exibir() { 
+        cout << "ID: " << Id << " Nome: " << Nome << " Titulo: " << Titulo << " Votou: " << (Votou ? "Sim" : "Nao") << endl; 
     }
 };
 
-class Sistema{
+class Candidato : public Pessoa {
+public:
+    int Numero;
+    string Partido;
+    Candidato(int id, int numero, int titulo, string nome, string partido) : Pessoa(id, titulo, nome), Numero(numero), Partido(partido) {}
+    void exibir() { 
+        cout << "Candidato " << Nome << " (" << Partido << ") - N° " << Numero << endl; 
+    }
+};
+
+struct Voto {
+    Candidato* candidato;
+    Eleitor* eleitor;
+    Voto(Candidato* c, Eleitor* e) : candidato(c), eleitor(e) {}
+};
+
+class Sistema {
+public:
     int Id;
     string Data, Descricao;
     bool Ativa;
-    list<Candidato> Candidatos;
-    list<Eleitor> Eleitores;
-    map<int, int> Votos;
+    vector<Candidato*> Candidatos;
+    vector<Eleitor*> Eleitores;
+    vector<Voto> Votos;
+    map<int, Eleitor*> DicEleitores;
+    map<int, Candidato*> DicCandidatos;
 
-    Sistema(int id, string data, string descricao){
-        Id = id;
-        Data = data;
-        Descricao = descricao;
-        Ativa = true;
+    Sistema(int id, string data, string descricao) : Id(id), Data(data), Descricao(descricao), Ativa(true) {}
+
+    void criarCandidato(Candidato* c) {
+        Candidatos.push_back(c);
+        DicCandidatos[c->Numero] = c;
+        cout << "Candidato adicionado: " << c->Nome << endl;
     }
 
-    void criarCandidato(Candidato candidato){
-        cout << "Candidato adicionado: " << candidato.Nome << endl;
-        Candidatos.push_back(candidato);
+    void criarEleitor(Eleitor* e) {
+        Eleitores.push_back(e);
+        DicEleitores[e->Id] = e;
+        cout << "Eleitor adicionado: " << e->Nome << endl;
     }
 
-    void criarEleitor(Eleitor eleitor){
-        cout << "Eleitor adicionado: " << eleitor.Nome << endl;
-    }
-
-    bool votou(int idEleitor, int numeroCandidato){
-        if (!Ativa){
-            cout << "acabou a votacao" << endl;
+    bool votou(int idEleitor, int numCandidato) {
+        if (!Ativa) {
+            cout << "Votacao encerrada" << endl;
             return false;
         }
-        else{
-            cout << "votacao ativa, pode votar" << endl;
 
-            for (Eleitor &eleitor : Eleitores){
-                if (eleitor.Id == idEleitor){
-                    if (eleitor.Votou){
-                        cout << "eleitor ja votou" << endl;
-                        return false;
-                    }
-                    else{
-                        eleitor.Votou = true;
-                        for (Candidato &candidato : Candidatos){
-                            if (candidato.Numero == numeroCandidato){
-                                candidato.VotosRecebidos++;
-                                Votos[idEleitor] = numeroCandidato;
-                                cout << "voto registrado para o candidato " << candidato.Nome << endl;
-                                return true;
-                            }
-                        }
-                        cout << "candidato nao encontrado" << endl;
-                        return false;
-                    }
-                }
-            }
+        auto itEleitor = DicEleitores.find(idEleitor);
+        if (itEleitor == DicEleitores.end()) {
+            cout << "Eleitor nao encontrado" << endl;
+            return false;
+        }
+
+        Eleitor* e = itEleitor->second;
+        if (e->Votou) {
+            cout << "Eleitor ja votou" << endl;
+            return false;
+        }
+
+        auto itCandidato = DicCandidatos.find(numCandidato);
+        if (itCandidato == DicCandidatos.end()) {
+            cout << "Candidato nao encontrado" << endl;
+            return false;
+        }
+
+        e->Votou = true;
+        Votos.emplace_back(itCandidato->second, e);
+        cout << "Voto registrado para " << itCandidato->second->Nome << endl;
+        return true;
+    }
+
+    void apurarVotacao() {
+        cout << "\nRESULTADO DA ELEICAO" << endl;
+        cout << "Descricao: " << Descricao << endl;
+        cout << "Total de votos: " << Votos.size() << "\n" << endl;
+
+        map<Candidato*, int> contagem;
+        for (auto& v : Votos) contagem[v.candidato]++;
+
+        vector<pair<Candidato*, int>> ordenado(contagem.begin(), contagem.end());
+        sort(ordenado.begin(), ordenado.end(), 
+             [](auto& a, auto& b) { return a.second > b.second; });
+
+        for (auto& [c, votos] : ordenado) {
+            cout << c->Nome << " (" << c->Partido << "): " << votos << " votos" << endl;
         }
     }
 };
+
+int main() {
+    Sistema sistema(1, "2026-10-01", "Eleicao Municipal");
+    int id, titulo, numero;
+    string nome, partido;
+
+    cout << "CANDIDATOS" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "\nCandidato " << i+1 << ":\nID: "; cin >> id;
+        cout << "Numero: "; cin >> numero; 
+        cout << "Titulo: "; cin >> titulo; 
+        cout << "Nome: "; cin.ignore(); getline(cin, nome);
+        cout << "Partido: ";  getline(cin, partido);
+        sistema.criarCandidato(new Candidato(id, numero, titulo, nome, partido));
+    }
+
+    cout << "\nELEITORES " << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "\nEleitor " << i+1 << ":\nID: "; cin >> id;
+        cout << "Titulo: "; cin >> titulo;  
+        cout << "Nome: ";cin.ignore(); getline(cin, nome);
+        sistema.criarEleitor(new Eleitor(id, titulo, nome));
+    }
+
+    cout << "\nVOTACAO " << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "\nID do Eleitor: "; cin >> id;
+        cout << "Numero do Candidato: "; cin >> numero;
+        sistema.votou(id, numero);
+    }
+
+    sistema.apurarVotacao();
+
+    return 0;
+}
---
+++ ex2.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Animais{
+public:
+    string Nome;
+    int Idade;
+    string Raca;
+    int Codigo;
+    vector<string> Especie;
+    Animais(string nome, int codigo, int idade, string raca, string tipo, string especie){
+    Nome = nome; Codigo = codigo; Idade = idade; Raca = raca; Especie.push_back(tipo); Especie.push_back(especie);
+    }
+
+    void registrar(){
+        cout << "Nome: " << Nome << endl;
+        cout << "Codigo: " << Codigo << endl;
+        cout << "Idade: " << Idade << endl;
+        cout << "Raca: " << Raca << endl;
+    }
+};
+
+class pessoa{
+public:
+    string Nome; string telefone; string Cpf; string Endereco;
+
+    pessoa(string nome, string cpf, string endereco){
+    Nome = nome; Cpf = cpf; Endereco = endereco;
+    }
+
+    void registrar(){
+        cout << "Nome: " << Nome << endl;
+        cout << "CPF: " << Cpf << endl;
+    }
+
+    void linkarAnimal(Animais animal){
+        cout << "Linkando animal " << animal.Nome << " com pessoa " << Nome << endl;
+    }
+};
+
+class funcionario : public pessoa{
+public:
+    string Funcao;
+    vector<Animais> AnimaisDeAtendimento;
+    int Codigo;
+
+    funcionario(string nome, string cpf, string endereco, string funcao, int codigo) : pessoa(nome, cpf, endereco){
+        Funcao = funcao;
+        Codigo = codigo;
+    }
+
+    void registrar(){
+        pessoa::registrar();
+        cout << "Funcao: " << Funcao << endl;
+        cout << "Codigo: " << Codigo << endl;
+    }
+
+    void linkarAnimal(Animais animal){
+        pessoa::linkarAnimal(animal);
+        AnimaisDeAtendimento.push_back(animal);
+    }
+};
+
+class cliente : public pessoa{
+public:
+    vector<Animais> animais;
+
+    cliente(string nome, string cpf, string endereco) : pessoa(nome, cpf, endereco) {};
+
+    void linkarAnimal(Animais animal){
+        pessoa::linkarAnimal(animal);
+        animais.push_back(animal);
+    }
+};
+
+class veterinaria{
+public:
+    string Nome;
+    vector<funcionario> funcionarios; vector<cliente> clientes; vector<Animais> animais;
+
+    veterinaria(string nome){
+        Nome = nome;
+    }
+
+    void registrarFuncionario(funcionario escravo){
+        funcionarios.push_back(escravo);
+    }
+
+    void registrarCliente(cliente fonteDinheiro){
+        clientes.push_back(fonteDinheiro);
+    }
+
+    void registrarAnimal(Animais animal){
+        animais.push_back(animal);
+    }
+};
+
+int main(){
+    int opcao, codigo, idade;
+    string nome, endereco, funcao, tipo, raca, especie, cpf;
+    veterinaria PS("Pet Shop");
+    do{
+        cout << "Menu" << endl;
+        cout << "1 - colocar Funcionario" << endl;
+        cout << "2 - colocar Cliente" << endl;
+        cout << "3 - colocar Animal" << endl;
+        cout << "0 - sair" << endl;
+        cout << "Escolha uma opcao: "; cin >> opcao;
+
+        switch (opcao){
+        case 1:{
+            cout << "Digite o nome do funcionario: "; cin.ignore(); getline(cin, nome);
+            cout << "Digite o CPF do funcionario: "; getline(cin, cpf);
+            cout << "Digite o endereco do funcionario: "; cin.ignore(); getline(cin, endereco);
+            cout << "Digite a funcao do funcionario: "; getline(cin, funcao);
+            cout << "Digite o codigo do funcionario: "; cin >> codigo;
+
+            funcionario escravo(nome, cpf, endereco, funcao, codigo);
+            PS.registrarFuncionario(escravo);
+            escravo.registrar();
+            break;
+        }
+        case 2:{
+            cout << "Digite o nome do cliente: "; cin.ignore(); getline(cin, nome);
+            cout << "Digite o CPF do cliente: "; getline(cin, cpf);
+            cout << "Digite o endereco do cliente: "; cin.ignore(); getline(cin, endereco);
+
+            cliente fonteDinheiro(nome, cpf, endereco);
+            PS.registrarCliente(fonteDinheiro);
+            fonteDinheiro.registrar();
+            break;
+        }
+        case 3:
+        {
+            cout << "Digite o nome do animal: "; cin.ignore(); getline(cin, nome);
+            cout << "Digite o codigo do animal: "; cin >> codigo;
+            cout << "Digite a idade do animal: "; cin >> idade;
+            cout << "Digite a raca do animal: "; cin.ignore(); getline(cin, raca);
+            cout << "Digite o tipo do animal: "; getline(cin, tipo);
+            cout << "Digite a especie do animal: "; getline(cin, especie);
+
+            Animais animal(nome, codigo, idade, raca, tipo, especie);
+            PS.registrarAnimal(animal);
+            animal.registrar();
+            break;
+        }
+        default:
+            if (opcao == 0){
+                cout << "codigo saido";
+                return 0;
+            }
+            else{
+                cout << "Opção inválida" << endl;
+            }
+        }
+    } while (opcao != 0);
+    return 0;
+}
